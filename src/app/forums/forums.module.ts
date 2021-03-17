4e699e94b694f629ce2e258bacc161910042cd36
Karl Lemfon <karllemfon@gmail.com>
2021-03-17 02:50:16 +0000

    final touches

--- src/app/forums/forums.module.ts
+++ src/app/forums/forums.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ClarityModule } from 'clarity-angular';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { ForumComponent } from './forum/forum.component';
 import { ForumsComponent } from './forums/forums.component';
@@ -10,6 +12,17 @@ import { ThreadComponent } from './thread/thread.component';
 import { ThreadsComponent } from './threads/threads.component';
 
 import { ForumsService } from './services/forums.service';
+
+const forumsRoutes: Routes = [
+  { path: 'forums', component: ForumsComponent },
+  { path: 'forums/:forum_alias',
+    component: ForumComponent,
+    children: [
+      {path: '', component: ThreadsComponent},
+      { path: ':thread_alias', component: ThreadComponent}
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
@@ -23,6 +36,7 @@ import { ForumsService } from './services/forums.service';
     FormsModule,
     HttpModule,
     ClarityModule.forChild(),
+    RouterModule.forChild(forumsRoutes),
   ],
   providers: [
     ForumsService
